cc6cbf3064e4521d76386e0470e80b5e760ac1f8
jiacai2050 <dev@liujiacai.net>
2025-11-09 19:10:24 +0800

    feat(shellgpt): uv run download-prompts.py

--- shellgpt/download-prompts.py
+++ shellgpt/download-prompts.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run --script
 # coding: utf-8
-
-# pip install tomli-w
+# /// script
+# dependencies = [
+#   "tomli-w",
+# ]
+# ///
 
 import csv
+import os
 import subprocess
+
 import tomli_w
 
 # https://github.com/f/awesome-chatgpt-prompts/
 url = 'https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv'
+PFILE = os.path.expanduser('./prompts.toml')
 
 
 def main():
-    out = subprocess.getoutput(f'curl -s {url}')
+    out = subprocess.getoutput(f'curl -LsSf {url}')
     rdr = csv.reader(out.split('\n'))
     contents = {}
     for row in rdr:
@@ -32,8 +38,9 @@ def main():
         content = row[1]
         contents[name] = content
 
-    with open('prompts.toml', 'wb') as f:
+    with open(PFILE, 'wb') as f:
         tomli_w.dump(contents, f)
+    print(f'Writing {len(contents)} prompts to {PFILE}')
 
 
 if __name__ == '__main__':
