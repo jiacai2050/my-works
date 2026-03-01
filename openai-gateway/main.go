97b8ddd3c795404b21abf627d1056ab05d11d2b5
jiacai2050 <dev@liujiacai.net>
2026-03-01 22:04:19 +0800

    feat(proxy): support dynamic auth header and verbose logging

--- openai-gateway/main.go
+++ openai-gateway/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -9,12 +11,24 @@ import (
 	"time"
 )
 
+var VERBOSE bool
+
 type loggingTransport struct {
 	transport http.RoundTripper
 }
 
 func (t *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	start := time.Now()
+	if VERBOSE {
+		var bodyBytes []byte
+		if req.Body != nil {
+			bodyBytes, _ = io.ReadAll(req.Body)
+			log.Printf("Request Body: %s", string(bodyBytes))
+			// Restore the io.ReadCloser to its original state
+			req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		}
+	}
+
 	resp, err := t.transport.RoundTrip(req)
 	duration := time.Since(start).Seconds()
 	log.Printf("[%d] %s %s (%.3fs)", resp.StatusCode, req.Method, req.URL.String(), duration)
@@ -24,15 +38,25 @@ func (t *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) 
 func main() {
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	baseUrl := os.Getenv("OPENAI_BASE_URL")
+	VERBOSE = os.Getenv("VERBOSE") == "1"
 	if baseUrl == "" {
 		baseUrl = "https://api.openai.com/v1"
 	}
 
 	upstream, _ := url.Parse(baseUrl)
+	authHeader := "Authorization"
+	// Cloudflare Gateway requires "cf-aig-authorization" header instead of "Authorization
+	// https://developers.cloudflare.com/ai-gateway/usage/chat-completion/
+	if upstream.Host == "gateway.ai.cloudflare.com" {
+		authHeader = "cf-aig-authorization"
+	}
+	if VERBOSE {
+		log.Printf("AuthHeader is %s", authHeader)
+	}
 	proxy := &httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetURL(upstream)
-			r.Out.Header.Set("Authorization", "Bearer "+apiKey)
+			r.Out.Header.Set(authHeader, "Bearer "+apiKey)
 		},
 		Transport: &loggingTransport{transport: http.DefaultTransport},
 	}
