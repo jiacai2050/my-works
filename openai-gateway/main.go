c9048e33fd281b15c15d8e7bd4adcd33822d3696
jiacai2050 <dev@liujiacai.net>
2026-03-02 20:15:47 +0800

    feat(openai-gateway): support log resp body

--- openai-gateway/main.go
+++ openai-gateway/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 	"time"
+)
+
+const (
+	authKey   = "authorization"
+	cfAuthKey = "cf-aig-authorization"
 )
 
 var VERBOSE bool
@@ -19,18 +26,34 @@ type loggingTransport struct {
 
 func (t *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	start := time.Now()
-	if VERBOSE {
+	if VERBOSE && req.Body != nil {
 		var bodyBytes []byte
-		if req.Body != nil {
-			bodyBytes, _ = io.ReadAll(req.Body)
-			log.Printf("Request Body: %s", string(bodyBytes))
-			// Restore the io.ReadCloser to its original state
-			req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-		}
+		bodyBytes, _ = io.ReadAll(req.Body)
+		log.Printf("Request Body: %s", string(bodyBytes))
+		// Restore the io.ReadCloser to its original state
+		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 	}
 
 	resp, err := t.transport.RoundTrip(req)
 	duration := time.Since(start).Seconds()
+
+	if VERBOSE && resp.Body != nil {
+		// 读取 Response 内容
+		respBytes, _ := io.ReadAll(resp.Body)
+		resp.Body = io.NopCloser(bytes.NewBuffer(respBytes))
+
+		contentEncoding := resp.Header.Get("Content-Encoding")
+		switch strings.ToLower(contentEncoding) {
+		case "gzip":
+			reader, _ := gzip.NewReader(bytes.NewBuffer(respBytes))
+			defer reader.Close()
+			decodedBytes, _ := io.ReadAll(reader)
+			log.Printf("Response Body (Decoded): %s", decodedBytes)
+		default:
+			log.Printf("Response Body: %s", respBytes)
+		}
+	}
+
 	log.Printf("[%d] %s %s (%.3fs)", resp.StatusCode, req.Method, req.URL.String(), duration)
 	return resp, err
 }
@@ -44,11 +67,11 @@ func main() {
 	}
 
 	upstream, _ := url.Parse(baseUrl)
-	authHeader := "Authorization"
+	authHeader := authKey
 	// Cloudflare Gateway requires "cf-aig-authorization" header instead of "Authorization
 	// https://developers.cloudflare.com/ai-gateway/usage/chat-completion/
 	if upstream.Host == "gateway.ai.cloudflare.com" {
-		authHeader = "cf-aig-authorization"
+		authHeader = cfAuthKey
 	}
 	if VERBOSE {
 		log.Printf("AuthHeader is %s", authHeader)
@@ -56,6 +79,9 @@ func main() {
 	proxy := &httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetURL(upstream)
+			if authHeader == cfAuthKey {
+				r.Out.Header.Del(authKey)
+			}
 			r.Out.Header.Set(authHeader, "Bearer "+apiKey)
 		},
 		Transport: &loggingTransport{transport: http.DefaultTransport},
